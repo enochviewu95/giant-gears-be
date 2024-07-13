7c40dcf24eaa84f21d4621789a1422f5e9eea9ab
enochviewu95 <viewuenoch@gmail.com>
2024-07-13 12:02:37 +0000

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    /**
+     * The function creates a new product using the validated data from a ProductRequest object.
+     * 
+     * @param ProductRequest productRequest The `ProductRequest` parameter in the `createProduct` function
+     * is likely a class that contains the validation rules and logic for validating incoming product data
+     * before creating a new product. It is commonly used in Laravel applications for form validation.
+     * 
+     * @return Product The `createProduct` function is returning an instance of the `Product` class.
+     */
+    public function createProduct(ProductRequest $productRequest): Product
+    {
+        $product = Product::create($productRequest->validated());
+        return $product;
+    }
+
+
+    /**
+     * The function `getProducts` returns an array of products in descending order of creation.
+     * 
+     * @return array An array of products is being returned, with the products sorted by the latest
+     * created date.
+     */
+    public function getProducts(): Collection
+    {
+        return Product::latest()->get();
+    }
+
+
+
+    /**
+     * The function getProduct takes a Product object as input and returns the same Product object.
+     * 
+     * @param Product product The `getProduct` function is a method that takes a `Product` object as a
+     * parameter and returns the same `Product` object. It simply accepts a `Product` object and then
+     * immediately returns it.
+     * 
+     * @return Product The `getProduct` function is returning the `` object of type `Product`.
+     */
+    public function getProduct(Product $product): Product
+    {
+        return $product;
+    }
+
+
+    /**
+     * The function `updateProduct` updates a product and returns a status message based on the success or
+     * failure of the update.
+     * 
+     * @param Product product The `updateProduct` function takes two parameters:
+     * @param ProductRequest productRequest The `updateProduct` function you provided takes two parameters:
+     * `` of type `Product` and `` of type `ProductRequest`.
+     * 
+     * @return array An array is being returned with a "status" key and a "message" key. The "status" key
+     * will have a value of either "success" or "fail" depending on the outcome of the update operation.
+     * The "message" key will provide additional information about the outcome, either "update successful"
+     * or "update failed".
+     */
+    public function updateProduct(Product $product, ProductRequest $productRequest): array
+    {
+        $response = $product->update($productRequest->validated());
+        if ($response) {
+            return [
+                "status" => "success",
+                "message" => "update successful"
+            ];
+        }
+        return [
+            "status" => "fail",
+            "message" => "update failed"
+        ];
+    }
+
+ /**
+  * The function `deleteProduct` deletes a product and returns a status message indicating success or
+  * failure.
+  * 
+  * @param Product product The `deleteProduct` function takes a `Product` object as a parameter and
+  * attempts to delete it from the database. If the deletion is successful, it returns an array with a
+  * "success" status and a message indicating that the delete was successful. If the deletion fails, it
+  * returns an array with
+  * 
+  * @return array An array is being returned with the keys "status" and "message" indicating the
+  * success or failure of the delete operation.
+  */
+    public function deleteProduct(Product $product): array
+    {
+        $response = $product->delete();
+        if ($response) {
+            return [
+                "status" => "success",
+                "message" => "delete successful"
+            ];
+        }
+        return [
+            "status" => "fail",
+            "message" => "delete failed"
+        ];
+    }
+
+
+    public function deletedSelectedProduct():string
+    {
+        return 'Deleted selected products';
+    }
+
+}
---
+++ routes/product.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(ProductController::class)->group(function () {
+
+    //Client routes for products
+    Route::prefix('client')->group(function () {
+        Route::get('/get-products', 'getProducts')
+            ->name('product.index');
+
+        Route::get('/get-product/{product}', 'getProduct')
+            ->name('product.show');
+    });
+
+
+    //Admin routes for products
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::prefix('admin')->group(function () {
+            Route::post('/create-product', 'createProduct')
+                ->middleware('auth:santum')
+                ->name('product.create');
+
+            Route::put('/update-product/{product}', 'updateProduct')
+                ->name('product.update');
+
+            Route::delete(
+                '/delete-product/{product}',
+                'deleteProduct'
+            )->name('product.delete');
+
+            Route::delete(
+                '/delete-products',
+                'deletedSelectedProduct'
+            )->name('product.selectDelete');
+        });
+    });
+});
+
+
+
+
+
